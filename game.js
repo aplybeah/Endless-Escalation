9b5c684ebcf828f8c4c4282b24a66e9298c796f3
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-04-11 22:49:39 -0400

    cors is upset]

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+const handleCORS = new XMLHttpRequest();
+handleCORS.open("GET", "/");
+handleCORS.setRequestHeader("Content-Type", "text/html");
+
 const config = {
   type: Phaser.AUTO,
   width: 800,
@@ -17,7 +21,14 @@ const config = {
 const game = new Phaser.Game(config);
 function preload() {
   this.load.setBaseURL("http://labs.phaser.io");
-  this.load.image("sky", "/assets/sky_purple.png");
+  this.load.image("sky", "assets/sky_purple.png");
+  this.load.image("ground", "assets/platform.png");
+  this.load.spritesheet("dude", "assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48
+  });
 }
-function create() {}
+function create() {
+  this.add.image(400, 300, "sky");
+}
 function update() {}
