2fc70551ef2760f5daec935a2560e324608bd62c
Alsia Plybeah <l.a.plybeah@gmail.com>
2019-04-11 22:19:07 -0400

    game screen renders in browser

--- game.js
+++ game.js
@@ -1,11 +1,22 @@
-const game = new Phaser.Game(800, 600, Phaser.AUTO, "", {
-  preload: preload,
-  create: create,
-  update: update
-});
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+const game = new Phaser.Game(config);
 function preload() {
-  game.load.image();
-  game.load.image();
+  this.load.setBaseURL("http://labs.phaser.io");
 }
 function create() {}
 function update() {}
